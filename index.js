d5f1117bd1e54f3d6682cb489505b9dab8a5afa2
David Turner <david@davidturner.name>
2014-07-08 10:02:08 +0100

    Actually implementing the mailgun logic.

--- index.js
+++ index.js
@@ -1,96 +1,65 @@
-/*
- * grunt-mailgun
- * http://github.com/markhuge/grunt-mailgun
- *
- * Copyright (c) 2014 Mark Wilkerson
- * Licensed under the MIT license.
- */
+// through2 is a thin wrapper around node transform streams
+var through = require('through2');
+var gutil = require('gulp-util');
+var Mailgun = require('mailgun').Mailgun;
+var PluginError = gutil.PluginError;
 
-/*
-var through = require('through2'),
-    gutil = require('gulp-util'),
-    rasterize = require('./lib'),
-    PluginError = gutil.PluginError;
+// Consts
+const PLUGIN_NAME = 'gulp-mailgun';
 
-const PLUGIN_NAME = 'gulp-raster';
-
-module.exports = function (opt) {
-    'use strict';
-
-    opt = opt || {};
-    opt.scale = opt.scale || 1;
-    opt.format = opt.format || 'png';
-
-    function raster(file, enc, callback) {
-        var that = this;
-
-        // Do nothing if no contents
-        if (file.isNull()) { return callback(); }
-
-        if (file.isBuffer()) {
-            rasterize(file.path, opt.format, opt.scale, function (err, data) {
-                if (err) { throw new PluginError(PLUGIN_NAME, 'Error occured during file conversion'); }
-
-                file.contents = data;
-                that.push(file);
-                return callback();
-            });
-        }
-    }
-
-    return through.obj(raster);
-};
-*/
-/*jslint node: true */
-// 'use strict';
-
-
-
-var mailer = require('mailgun-send'),
-    _      = require('lodash');
-
-
-var sendmail = function (opts) {
-  console.log(opts);
+function mailgunStream(mailgunText) {
+  'use strict';
+  var stream = through();
+  stream.write(mailgunText);
+  return stream;
 }
 
-// module.exports = function (grunt) {
-// module.exports = function (opt) {
-//     'use strict';
+// Plugin level function(dealing with files)
+function gulpMailgun(mailgunText) {
+  'use strict';
+  var _opts = mailgunText;
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+  if (!mailgunText) {
+    throw new PluginError(PLUGIN_NAME, 'Missing prefix text!');
+  }
+  mailgunText = new Buffer(mailgunText); // allocate ahead of time
 
+  // Creating a stream through which each file will pass
+  var stream = through.obj(function(file, enc, callback) {
+    var mail, raw;
+    if (file.isNull()) {
+       // Do nothing if no contents
+      _opts.body = '';
+    }
+    if (file.isBuffer()) {
+      file.contents = Buffer.concat([mailgunText, file.contents]);
+      _opts.body = file.contents;
+    }
 
-  // console.log(opt);
+    if (file.isStream()) {
+      file.contents = file.contents.pipe(mailgunStream(mailgunText));
+      _opts.body = file.contents;
+    }
+    mail = new Mailgun(_opts.key);
 
+    raw = "From: " + _opts.sender + "\nTo: " + _opts.recipient + "\nContent-Type: text/html; charset=utf-8\nSubject: " + _opts.subject + "\n\n " + _opts.body;
+    mail.sendRaw(_opts.sender, _opts.recipient, raw, function(err) {
+      if (err) {
+        console.log('Please make sure you have entered your Mailgun API Key in your gulpfile');
+        throw new PluginError(PLUGIN_NAME, 'Error: ' + err);
+      } else {
+        console.log('Sent email to ' + _opts.recipient);
+      }
+    });
 
-  // grunt.registerMultiTask('mailgun', 'Send emails through mailgun as part of your build.', function () {
-  //   var done = this.async();
-  //   var opts = _.pick(this.data.options,['sender','recipient','subject','body']);
-  //
-  //   // Register our mailer instance with out API key
-  //   mailer.config({key: this.data.options.key});
-  //
-  //   if (this.files.length > 0) {
-  //     var i = this.files.length;
-  //     this.filesSrc.forEach(function (filePath) {
-  //       var _opts = _.clone(opts);
-  //       _opts.body = grunt.file.read(filePath);
-  //       mailer.send(_opts, function () {
-  //         grunt.log.writeln('Sent' + filePath + ' to ' + _opts.recipient);
-  //         if (i < 1) { done(); } else { i--; } // This seems dirty
-  //       });
-  //
-  //     });
-  //
-  //   } else {
-  //     mailer.send(opts, function () {
-  //       grunt.log.writeln('Sent mailgun msg to ' + opts.recipient);
-  //       done();
-  //     });
-  //      }
-  //
-  // });
+    this.push(file);
+    return callback();
 
-// };
+  });
+
+  // returning the file stream
+  return stream;
+}
+
+// Exporting the plugin main function
+module.exports = gulpMailgun;
